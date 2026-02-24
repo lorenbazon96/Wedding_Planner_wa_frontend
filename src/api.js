5e6407bd783903708726119de415a348054b5585
lorenbazon96 <lbazon@unipu.hr>
2026-02-24 11:23:48 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
-const api = axios.create({ baseURL: API_URL });
+const api = axios.create({
+  baseURL: `${API_URL}/api`,
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
